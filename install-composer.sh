fc2704cbb5c3d08489d6858736ba5db2238f92c7
Aykut Yakan <aykutyakan@gmail.com>
2020-02-21 11:36:48 +0300

    creating the compoer
    using all install command for composer installing

---
+++ install-composer.sh
@@ -0,0 +1,6 @@
+sudo su
+apt-get update
+apt-get install wget php-cli php-zip unzip
+HASH="$(wget -q -O - https://composer.github.io/installer.sig)"
+php -r "if (hash_file('SHA384', 'composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php --install-dir=/usr/local/bin --filename=composer
